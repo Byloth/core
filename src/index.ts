c303c4f3e08e0be105ab443138aa2421d9ec5c3a
Matteo Bilotta <matteo@dinobros.com>
2023-10-04 03:33:47 +0200

    fix: Inverted the logic for the `DeferredPromise` class.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export const VERSION = "1.1.1";
+export const VERSION = "1.1.2";
 
 export { DeferredPromise, Exception, JsonStorage, Subscribers } from "./models/index.js";
 export {
--- src/models/deferred-promise.ts
+++ src/models/deferred-promise.ts
@@ -1,29 +1,27 @@
-import type {
-    PromiseExecutor,
-    PromiseResolver,
-    PromiseRejecter,
-    FulfilledHandler,
-    RejectedHandler,
-    MaybePromise
+import type { PromiseResolver, PromiseRejecter, FulfilledHandler, RejectedHandler, MaybePromise } from "../types.js";
 
-} from "../types.js";
-
-export default class DeferredPromise<T>
+export default class DeferredPromise<T, E = never>
 {
     protected _resolve!: PromiseResolver<T>;
     protected _reject!: PromiseRejecter;
 
-    protected _promise: Promise<T>;
+    protected _promise: Promise<T | E>;
 
-    public constructor(executor?: PromiseExecutor<T>)
+    public constructor(onFulfilled?: FulfilledHandler<unknown, T>, onRejected?: RejectedHandler<unknown, E>)
     {
-        this._promise = new Promise((resolve, reject) =>
-        {
-            this._resolve = resolve;
-            this._reject = reject;
+        let _resolve: PromiseResolver<T>;
+        let _reject: PromiseRejecter;
 
-            executor?.(resolve, reject);
+        const _promise = new Promise<T | E>((resolve, reject) =>
+        {
+            _resolve = resolve;
+            _reject = reject;
         });
+
+        this._promise = _promise.then(onFulfilled, onRejected);
+
+        this._resolve = _resolve!;
+        this._reject = _reject!;
     }
 
     public resolve(value: MaybePromise<T>)
@@ -35,16 +33,17 @@ export default class DeferredPromise<T>
         this._reject(reason);
     }
 
-    public then<F = T, R = never>(onFulfilled?: FulfilledHandler<T, F>, onRejected?: RejectedHandler<unknown, R>)
-        : Promise<F | R>
+    public then<F = T | E, R = never>(
+        onFulfilled?: FulfilledHandler<T | E, F>,
+        onRejected?: RejectedHandler<unknown, R>): Promise<F | R>
     {
         return this._promise.then(onFulfilled, onRejected);
     }
-    public catch<R = never>(onRejected?: RejectedHandler<unknown, R>): Promise<T | R>
+    public catch<R = never>(onRejected?: RejectedHandler<unknown, R>): Promise<T | E | R>
     {
         return this._promise.catch(onRejected);
     }
-    public finally(onFinally?: () => void): Promise<T>
+    public finally(onFinally?: () => void): Promise<T | E>
     {
         return this._promise.finally(onFinally);
     }
