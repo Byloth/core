9f9fd6fc1214b0a9459c6f69b0e2ff7f6d935ece
Matteo Bilotta <matteo@dinobros.com>
2023-10-13 14:31:37 +0200

    fix: Now DeferredPromise can be casted into Promise without throwing TS errors.

--- src/models/deferred-promise.ts
+++ src/models/deferred-promise.ts
@@ -7,7 +7,7 @@ export default class DeferredPromise<T = void, E = unknown, F = T, R = never>
 
     protected _promise: Promise<F | R>;
 
-    public constructor(onFulfilled?: FulfilledHandler<T, F>, onRejected?: RejectedHandler<E, R>)
+    public constructor(onFulfilled?: FulfilledHandler<T, F> | null, onRejected?: RejectedHandler<E, R> | null)
     {
         let _resolve: PromiseResolver<T>;
         let _reject: PromiseRejecter<E>;
@@ -33,17 +33,23 @@ export default class DeferredPromise<T = void, E = unknown, F = T, R = never>
         return this._reject;
     }
 
-    public then<N = F | R, H = R>(onFulfilled?: FulfilledHandler<F | R, N>, onRejected?: RejectedHandler<R, H>)
-        : Promise<N | H>
+    public then<N = F | R, H = R>(
+        onFulfilled?: FulfilledHandler<F | R, N> | null,
+        onRejected?: RejectedHandler<R, H> | null): Promise<N | H>
     {
         return this._promise.then(onFulfilled, onRejected);
     }
-    public catch<H = R>(onRejected?: RejectedHandler<R, H>): Promise<F | R | H>
+    public catch<H = R>(onRejected?: RejectedHandler<R, H> | null): Promise<F | R | H>
     {
         return this._promise.catch(onRejected);
     }
-    public finally(onFinally?: () => void): Promise<F | R>
+    public finally(onFinally?: (() => void) | null): Promise<F | R>
     {
         return this._promise.finally(onFinally);
+    }
+
+    public get [Symbol.toStringTag]()
+    {
+        return "DeferredPromise";
     }
 }
