b6f85ca44ff3ee9d1cc858cebc2bec25d01918a2
Matteo Bilotta <me@byloth.dev>
2024-11-04 15:44:30 +0100

    imp: Added same publishers for `Clock` object.

--- src/models/timers/clock.ts
+++ src/models/timers/clock.ts
@@ -6,13 +6,17 @@ import Publisher from "../publisher.js";
 
 export default class Clock extends GameLoop
 {
-    protected _publisher: Publisher<[number]>;
+    protected _starter: Publisher;
+    protected _stopper: Publisher;
+    protected _ticker: Publisher<[number]>;
 
     public constructor(msIfNotBrowser: number = TimeUnit.Second)
     {
-        super((elapsedTime) => this._publisher.publish(elapsedTime), msIfNotBrowser);
+        super((elapsedTime) => this._ticker.publish(elapsedTime), msIfNotBrowser);
 
-        this._publisher = new Publisher();
+        this._starter = new Publisher();
+        this._stopper = new Publisher();
+        this._ticker = new Publisher();
     }
 
     public start(elapsedTime = 0): void
@@ -20,6 +24,8 @@ export default class Clock extends GameLoop
         if (this._isRunning) { throw new RuntimeException("The clock has already been started."); }
 
         super.start(elapsedTime);
+
+        this._starter.publish();
     }
 
     public stop(): void
@@ -27,16 +33,27 @@ export default class Clock extends GameLoop
         if (!(this._isRunning)) { throw new RuntimeException("The clock hadn't yet started."); }
 
         super.stop();
+
+        this._stopper.publish();
+    }
+
+    public onStart(callback: () => void): () => void
+    {
+        return this._starter.subscribe(callback);
+    }
+    public onStop(callback: () => void): () => void
+    {
+        return this._stopper.subscribe(callback);
     }
 
     public onTick(callback: (elapsedTime: number) => void, tickStep = 0): () => void
     {
         if (tickStep < 0) { throw new RangeException("The tick step must be a non-negative number."); }
-        if (tickStep === 0) { return this._publisher.subscribe(callback); }
+        if (tickStep === 0) { return this._ticker.subscribe(callback); }
 
         let lastTick = 0;
 
-        return this._publisher.subscribe((elapsedTime: number) =>
+        return this._ticker.subscribe((elapsedTime: number) =>
         {
             if ((elapsedTime - lastTick) < tickStep) { return; }
 
