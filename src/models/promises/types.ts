1c93cc61a56502d2f38122962f3fe921add9e203
Matteo Bilotta <me@byloth.dev>
2024-12-29 01:03:00 +0100

    del: Removed unfinished & untested features.

--- src/models/promises/types.ts
+++ src/models/promises/types.ts
@@ -1,5 +1,3 @@
-export type { LongRunningTaskOptions } from "./long-running-task.js";
-
 export type MaybePromise<T> = T | PromiseLike<T>;
 
 export type FulfilledHandler<T = void, R = T> = (value: T) => MaybePromise<R>;
