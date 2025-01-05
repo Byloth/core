b5472f7d0d7b30232b1ed370d350aa4bc7a5ab6b
Matteo Bilotta <me@byloth.dev>
2025-01-05 09:40:43 +0100

    fix: Fixed a no longer required `eslint-disable` rule.

--- src/models/json/types.ts
+++ src/models/json/types.ts
@@ -6,7 +6,6 @@ export type JSONArray = JSONValue[];
 /**
  * A type representing a JSON object.
  */
-// eslint-disable-next-line @typescript-eslint/consistent-indexed-object-style
 export interface JSONObject { [key: string]: JSONValue }
 
 /**
