14b41c363fd95ff2091fe0afabd3b4a271195e1b
Matteo Bilotta <me@byloth.dev>
2025-01-31 14:24:46 +0100

    upd: Minor update.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,9 +9,9 @@ const __dirname = path.dirname(__filename);
 const gitignorePath = path.resolve(__dirname, ".gitignore");
 
 export default [includeIgnoreFile(gitignorePath), ...eslintTs, {
-    rules: {
-        "no-trailing-spaces": ["error", { "ignoreComments": true }],
-        "@typescript-eslint/no-non-null-assertion": "off",
-        "@typescript-eslint/unified-signatures": "off"
-    }
+  rules: {
+    "no-trailing-spaces": ["error", { "ignoreComments": true }],
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/unified-signatures": "off"
+  }
 }];
