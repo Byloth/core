93f70344a5111059ea415fea62ed7696064e9d74
Matteo Bilotta <me@byloth.dev>
2025-04-20 12:35:18 +0200

    fix: Better support for `unpkg` & `jsdevilr`.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,16 @@ export default defineConfig({
   build: {
     lib: {
       entry: fileURLToPath(new URL("src/index.ts", import.meta.url)),
+      fileName: (format) =>
+      {
+        if (format === "cjs") { return `core.cjs`; }
+        if (format === "es") { return `core.esm.js`; }
+        if (format === "iife") { return `core.global.js`; }
+        if (format === "umd") { return `core.umd.cjs`; }
+
+        throw new Error(`Unknown build format: ${format}`);
+      },
+      formats: ["cjs", "es", "iife", "umd"],
       name: "Core"
     },
     rollupOptions: {
